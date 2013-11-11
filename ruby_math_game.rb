03b84274413f5ae8d062dc72ebed10b5c96e0ed5
Pierre Beugnot <pierrebeugnot@gmail.com>
2013-11-10 21:50:31 -0800

    Fix some errors - comments in the code

--- ruby_math_game.rb
+++ ruby_math_game.rb
@@ -18,18 +18,28 @@ def get_answer(operator, x, y)
    else 
     operator = " x "
   end
+  # chomp() doesn't take arguments. You need to echo first and then "listen"
+  # for the users input
+  # http://ruby.learncodethehardway.org/book/ex14.html
+  puts 'Player' + @current_player.to_s + ': What does ' + x.to_s + operator + y.to_s + ' equal?'
+  return gets.chomp()
 end
+
  # part of the below line is changed into Ruby but i'm not sure of syntax.
-  return gets.chomp('Player' + @current_player.to_s + ': What does ' + x + operator + y + ' equal?')
+
+#gets.chomp('Player' + @current_player.to_s + ': What does ' + x + operator + y + ' equal?')
 
  
 def switch_player() 
   if (@current_player == 1) 
     @current_player = 2
-    puts("You have " + @player1_lives + " lives remaining")
+    #/!\ Attention, you can't mix numbers type and string type without
+    # casting them
+    puts("You have " + @player1_lives.to_s + " lives remaining")
    else 
     @current_player = 1
-    puts("You have " + @player2_lives + " lives remaining")
+    #same here
+    puts("You have " + @player2_lives.to_s + " lives remaining")
   end
 end
  
@@ -52,31 +62,38 @@ def announce_winner()
 end
  
 def check_answer(answer, right_answer) 
-  if (answer == right_answer) 
+  # same here you can't compare a string and a number
+  # it's either string == string or
+  # number == number
+  if (answer.to_s == right_answer.to_s) 
     puts("Correct.")
    else 
     if (@current_player == 1) 
-      player1_lives -= 1
-      puts("Wrong! " + @player1_lives + " lives left!")
+      #missing @
+      @player1_lives -= 1
+      # wrong type again
+      puts("Wrong! " + @player1_lives.to_s + " lives left!")
      else 
-      player2_lives -= 1
-      puts("Wrong! " + @player2_lives + " lives left!")
+      #missing @
+      @player2_lives -= 1
+      puts("Wrong! " + @player2_lives.to_s + " lives left!")
     end
   end
 end
 
- 
+# wrong while syntax
+# breaks the game logic
+while (check_lives() == true)
   x  = rand(20) + 1
   y  = rand(20) + 1
   op = rand(3) + 1
-  
+
   right_answer = calculate_answer(op, x, y)
   answer = get_answer(op, x, y)
- 
+
   check_answer(answer, right_answer)
   switch_player()
- 
- while (check_lives() == true )
- 
-announce_winner()
+  
 end
+
+announce_winner()
