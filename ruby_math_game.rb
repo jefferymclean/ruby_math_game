d1d4da2f031eb9bc7ba928dea838e7f61884a128
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-12 21:04:11 +0000

    final commit bitches

--- ruby_math_game.rb
+++ ruby_math_game.rb
@@ -3,11 +3,7 @@
 @current_player = 1
  
 def check_lives() 
-  if ((@player1_lives > 0) && (@player2_lives > 0)) 
-    return true
-   else 
-    return false
-  end
+  (@player1_lives > 0) && (@player2_lives > 0) 
 end
  
 def get_answer(operator, x, y) 
@@ -18,21 +14,12 @@ def get_answer(operator, x, y)
    else 
     operator = " x "
   end
-end
- # part of the below line is changed into Ruby but i'm not sure of syntax.
-  return gets.chomp('Player' + @current_player.to_s + ': What does ' + x + operator + y + ' equal?')
 
- 
-def switch_player() 
-  if (@current_player == 1) 
-    @current_player = 2
-    puts("You have " + @player1_lives + " lives remaining")
-   else 
-    @current_player = 1
-    puts("You have " + @player2_lives + " lives remaining")
-  end
+  puts "Player #{@current_player}: What does #{x}#{operator}#{y} equal?"
+  gets.chomp.to_i
 end
- 
+
+
 def calculate_answer(op, x, y) 
   if (op == 1) 
     return x + y
@@ -43,11 +30,23 @@ def calculate_answer(op, x, y)
   end
 end
  
+def switch_player() 
+  if (@current_player == 1) 
+    @current_player = 2
+    puts("You have " + @player1_lives.to_s + " lives remaining")
+   else 
+    @current_player = 1
+    puts("You have " + @player2_lives.to_s + " lives remaining")
+  end
+end
+ 
+ 
 def announce_winner() 
   if (@player_1Lives == 0) 
     puts("Player 2 wins!")
    else 
     puts("Player 1 wins!")
+
   end
 end
  
@@ -56,27 +55,35 @@ def check_answer(answer, right_answer)
     puts("Correct.")
    else 
     if (@current_player == 1) 
-      player1_lives -= 1
-      puts("Wrong! " + @player1_lives + " lives left!")
+      @player1_lives -= 1
+      puts("Wrong! " + @player1_lives.to_s + " lives left!")
      else 
-      player2_lives -= 1
-      puts("Wrong! " + @player2_lives + " lives left!")
+      @player2_lives -= 1
+      puts("Wrong! " + @player2_lives.to_s + " lives left!")
     end
   end
 end
 
  
-  x  = rand(20) + 1
-  y  = rand(20) + 1
-  op = rand(3) + 1
   
-  right_answer = calculate_answer(op, x, y)
-  answer = get_answer(op, x, y)
- 
-  check_answer(answer, right_answer)
-  switch_player()
- 
- while (check_lives() == true )
- 
-announce_winner()
-end
+
+  while (@player1_lives > 0) && (@player2_lives > 0) 
+
+    x  = rand(20) + 1
+    y  = rand(20) + 1
+    op = rand(3) + 1
+
+    right_answer = calculate_answer(op, x, y)
+    answer = get_answer(op, x, y)
+   
+    check_answer(answer, right_answer)
+    switch_player()
+
+  end 
+
+    announce_winner()
+    #break
+
+
+    
+ # end
