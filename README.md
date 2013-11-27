3a576ce21ef21996203090ff131a6e2e806f3336
Jeffery McLean <jeffery.mclean@gmail.com>
2013-11-27 03:28:17 +0000

    create read me

---
+++ README.md
@@ -0,0 +1,3 @@
+### Ruby Math Game 
+
+This is the same two player math game that I created in week 2 - except with one significant difference: I've changed it from JavaScript to Ruby.
--- ruby_math_game.rb
+++ ruby_math_game.rb
@@ -9,9 +9,9 @@ end
 def get_answer(operator, x, y) 
   if (operator == 1) 
     operator = " + "
-   elsif (operator == 2) 
+  elsif (operator == 2) 
     operator = " - "
-   else 
+  else 
     operator = " x "
   end
 
@@ -23,9 +23,9 @@ end
 def calculate_answer(op, x, y) 
   if (op == 1) 
     return x + y
-   elsif (op == 2) 
+  elsif (op == 2) 
     return x - y
-   else 
+  else 
     return x * y
   end
 end
@@ -34,7 +34,7 @@ def switch_player()
   if (@current_player == 1) 
     @current_player = 2
     puts("You have " + @player1_lives.to_s + " lives remaining")
-   else 
+  else 
     @current_player = 1
     puts("You have " + @player2_lives.to_s + " lives remaining")
   end
@@ -44,20 +44,19 @@ end
 def announce_winner() 
   if (@player_1Lives == 0) 
     puts("Player 2 wins!")
-   else 
+  else 
     puts("Player 1 wins!")
-
   end
 end
  
 def check_answer(answer, right_answer) 
   if (answer == right_answer) 
     puts("Correct.")
-   else 
+  else 
     if (@current_player == 1) 
       @player1_lives -= 1
       puts("Wrong! " + @player1_lives.to_s + " lives left!")
-     else 
+    else 
       @player2_lives -= 1
       puts("Wrong! " + @player2_lives.to_s + " lives left!")
     end
@@ -67,23 +66,21 @@ end
  
   
 
-  while (@player1_lives > 0) && (@player2_lives > 0) 
+while (@player1_lives > 0) && (@player2_lives > 0) 
 
-    x  = rand(20) + 1
-    y  = rand(20) + 1
-    op = rand(3) + 1
+  x  = rand(20) + 1
+  y  = rand(20) + 1
+  op = rand(3) + 1
 
-    right_answer = calculate_answer(op, x, y)
-    answer = get_answer(op, x, y)
-   
-    check_answer(answer, right_answer)
-    switch_player()
+  right_answer = calculate_answer(op, x, y)
+  answer = get_answer(op, x, y)
+ 
+  check_answer(answer, right_answer)
+  switch_player()
 
-  end 
+end 
 
-    announce_winner()
-    #break
+announce_winner()
+  
 
-
-    
- # end
+  
